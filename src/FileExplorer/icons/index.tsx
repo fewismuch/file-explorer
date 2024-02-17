72e77a7e5d47b3b09bf93a28979b7cc33a5f7e54
tianyu <779205344@qq.com>
2024-02-17 23:41:29 +0800

    feat: init

--- src/FileExplorer/icons/index.tsx
+++ src/FileExplorer/icons/index.tsx
@@ -1,10 +1,44 @@
+import {
+  CheckOutlined,
+  CloseOutlined,
+  DeleteFilled,
+  EditFilled,
+  FileAddFilled,
+  FileFilled,
+  FolderAddFilled,
+  FolderFilled,
+  FolderOpenFilled,
+  RightOutlined,
+} from '@ant-design/icons'
 import React from 'react'
 
-const Icon: React.FC<any> = (props) => {
+const IconMap = {
+  file: <FileFilled />,
+  folder: <FolderFilled />,
+  addFile: <FileAddFilled />,
+  addFolder: <FolderAddFilled />,
+  delete: <DeleteFilled />,
+  edit: <EditFilled />,
+  folderOpen: <FolderOpenFilled />,
+  right: <RightOutlined />,
+  close: <CloseOutlined />,
+  check: <CheckOutlined />,
+}
+
+interface IIcon {
+  name: keyof typeof IconMap
+  style?: {
+    color?: string
+    fontSize?: string | number
+  }
+  [key: string]: any
+}
+
+const Icon: React.FC<IIcon> = (props) => {
   const { style, name, ...rest } = props
   return (
     <span {...rest} style={{ display: 'flex', alignItems: 'center', ...style }}>
-      +
+      {IconMap[name]}
     </span>
   )
 }
